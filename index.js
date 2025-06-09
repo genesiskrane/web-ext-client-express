892b1e11bb3b1584b1539991714f559eace80b90
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-09 21:50:12 +0100

    Index Modified

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 require("./config");
-
 const { init } = require("./core");
 
 const path = require("path");
 const fs = require("fs");
-
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
 
-// HTTPS Redirect
+// Redirect HTTP to HTTPS (Production)
 app.use((req, res, next) => {
-	if (process.env.NODE_ENV == "production")
+	if (process.env.NODE_ENV === "production") {
 		if (req.headers["x-forwarded-proto"] !== "https") {
 			return res.redirect(`https://${req.headers.host}${req.url}`);
 		}
+	}
 	next();
 });
 
 app.set("trust proxy", true);
-
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Catch All
+// Middleware to serve static assets based on TLD
+app.use((req, res, next) => {
+	const ext = req.hostname.split(".").reverse()[0]; // e.g., 'store' or 'pro'
+	const basePath = path.join(__dirname, "client", ext, "dist");
+
+	if (fs.existsSync(basePath)) {
+		express.static(basePath)(req, res, next);
+	} else {
+		res.status(404).send("Not Found");
+	}
+});
+
 app.all("/{*any}", (req, res) => {
 	const ext = req.hostname.split(".").reverse()[0];
+	const indexPath = path.join(__dirname, "client", ext, "dist", "index.html");
 
-	const extPath = path.join(__dirname, "client", ext);
-	const indexPath = path.join(extPath, "dist", "index.html");
-
-	if (fs.existsSync(indexPath))
-		express.static(path.join(__dirname, "client", ext))(req, res, next);
-	else res.status(404).send("Not Found");
+	if (fs.existsSync(indexPath)) {
+		res.sendFile(indexPath);
+	} else res.status(404).send("Not Found");
 });
 
 (async () => {
