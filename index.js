2593ad2529698e506890652206004a25ba4e8997
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-09 15:29:05 +0100

    Index.html Domains Added

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-app.use(express.static(path.join(__dirname, "client", "dist")));
+app.use(express.static(path.join(__dirname, "client", "domains", ".pro")));
 
 // Catch All
 app.all("/{*any}", (req, res) => {
