03a1ae278d858f721f32805250ceff822355f5dd
Augment Chinedu <augmentchinedu@gmail.com>
2025-06-09 19:46:19 +0100

    Building

--- build.sh
+++ build.sh
@@ -3,3 +3,21 @@ npm install
 mkdir 'client'
 
 ls
+
+
+# URL to your JSON file
+
+
+# Fetch the JSON content
+REPOS_JSON=$(curl -s "$PROD_GKRANE_URL/$APPNAME")
+
+echo $REPO_JSON
+
+# # Parse JSON and extract repo URLs using jq
+# REPOS=$(echo "$REPOS_JSON" | jq -r '.repositories[]')
+
+# # Loop through each repo and do something (e.g., clone)
+# for REPO in $REPOS; do
+#   echo "Cloning $REPO"
+#   git clone "$REPO"
+# done
